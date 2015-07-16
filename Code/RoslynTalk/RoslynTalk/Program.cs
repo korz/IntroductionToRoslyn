5417ef66a45701cb8b62a86f3b191c0c69295627
korz <korzy1bj@gmail.com>
2015-07-16 15:22:36 -0400

    initial code commit

---
+++ Code/RoslynTalk/RoslynTalk/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+
+namespace RoslynTalk
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string code = File.ReadAllText("Customer.cs");
+
+            var analyzer = new SimpleCodeAnalyzer();
+
+            analyzer.Analyse();
+
+            Console.ReadKey();
+        }
+    }
+}
